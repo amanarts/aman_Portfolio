863e0528cf4e5bffb33f8bffa942b4346d21aaed
amanarts <amanarts007@gmail.com>
2022-02-22 01:30:49 +0530

    created a folder structure

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,9 +1,1 @@
-import NavigationDots from './NavigationDots';
-import SocialMedia from './SocialMedia';
-import Navbar from './Navbar/Navbar';
-
-export {
-  NavigationDots,
-  SocialMedia,
-  Navbar,
-};
+export {default as Navbar} from './Navbar/Navbar'
